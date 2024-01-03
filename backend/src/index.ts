9263d56e7e6b10a2403498c377c4b05cc7748b4c
vvercasson <vercasson1605@gmail.com>
2024-01-03 16:40:10 +0100

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 const app = express();
+import cors from 'cors';
 
 // Import routers
 const cocktailRoutes = require('./routes/cocktailsRoutes.js');
 
 const PORT = 80;
+
+app.use(cors());
 
 app.use('/api/cocktails', cocktailRoutes);
 
