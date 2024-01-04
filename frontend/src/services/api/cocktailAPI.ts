0b73fa91773b0c3b5af021c6d33f4c8e0d96b8b2
vvercasson <vercasson1605@gmail.com>
2024-01-04 15:45:20 +0100

    add ingredient

--- frontend/src/services/api/cocktailAPI.ts
+++ frontend/src/services/api/cocktailAPI.ts
@@ -1,4 +1,5 @@
 import { Cocktail } from '../../model/Cocktail';
+import { Ingredient } from '../../model/Ingredient';
 
 const API_URL = 'http://localhost:3001/api/cocktails/';
 
@@ -6,15 +7,34 @@ interface ApiCocktailResponse {
     drinks: Cocktail[];
 }
 
-// Data to cocktail object
+const mapIngredient = (drink: any): Ingredient[] => {
+    const ingredients: Ingredient[] = [];
+
+    for (let i = 1; i <= 15; i++) {
+        const ingredientName = drink[`strIngredient${i}`];
+        const ingredientMeasure = drink[`strMeasure${i}`];
+
+        if (ingredientName && ingredientMeasure) {
+            ingredients.push({
+                strIngredient: ingredientName,
+                strMeasure: ingredientMeasure
+            });
+        }
+    }
+
+    return ingredients;
+}
+
+
 const mapCocktail = (data: ApiCocktailResponse) => {
     const relevantData = data.drinks.map(drink => ({
         idDrink: drink.idDrink,
         strDrink: drink.strDrink,
         strDrinkThumb: drink.strDrinkThumb,
         strInstructions: drink.strInstructions,
-        IngredientList : []
+        IngredientList: mapIngredient(drink)
     }));
+    console.log(relevantData);
 
     return relevantData;
 }
@@ -27,11 +47,11 @@ export const fetchRandomCocktails = async (num: number) => {
 
     for (let i = 0; i < num; i++) {
         const response = await fetch(RANDOM_API_URL);
-        if(!response.ok) throw new Error('Error fetching random cocktails');
+        if (!response.ok) throw new Error('Error fetching random cocktails');
 
         const data: ApiCocktailResponse = await response.json();
 
-        if(data && data.drinks && data.drinks.length > 0) {
+        if (data && data.drinks && data.drinks.length > 0) {
             const relevantData = mapCocktail(data);
             cocktailsList.push(...relevantData);
         }
@@ -45,11 +65,11 @@ export const fetchCocktailByName = async (name: string) => {
     const NAME_API_URL = API_URL + `search/name/${name}`;
 
     const response = await fetch(NAME_API_URL);
-    if(!response.ok) throw new Error('Error fetching cocktail by name');
+    if (!response.ok) throw new Error('Error fetching cocktail by name');
 
     const data: ApiCocktailResponse = await response.json();
 
-    if(data && data.drinks && data.drinks.length > 0) {
+    if (data && data.drinks && data.drinks.length > 0) {
         const relevantData = mapCocktail(data);
         return relevantData;
     }
