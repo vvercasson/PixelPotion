869a8414d6569ac14ac8444a7d1e4cd29576a146
aducos003 <alex.mannant@gmail.com>
2024-01-11 18:39:02 +0100

    avancement de la doc swagger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,8 @@ import router from "./routes/routeIndex";
 import cors from "cors";
 import RedisCache from "./middleware/redis.cache";
 import bodyParse from "body-parser";
+import swaggerJsDoc from 'swagger-jsdoc';
+import {serve, setup} from 'swagger-ui-express'
 
 import { SQLiteDatabase } from "./database/database.config";
 export class App {
@@ -17,12 +19,36 @@ export class App {
         this.initializeMiddlewares();
         this.initializeCache();
         this.initializeRoutes(router);
+        this.initializeSwagger();
     }
 
     private initializeMiddlewares() {
         this.app.use(cors());
         this.app.use(bodyParse.json({ limit: '10mb' }));
         this.app.use(bodyParse.urlencoded({ limit: '10mb', extended: true }));
+    }
+
+    private initializeSwagger() {
+        const options = {
+            definition: {
+                openapi: "3.0.0",
+                info: {
+                    title: "PixelPotion API",
+                    version: "0.1",
+                    description: "An API to access a collection of cocktail recipes. It provides functionalities to search for cocktails by name, ingredient, ID, and retrieve random information.",
+                },
+            },
+            /* servers: [
+                {
+                    url: 'http://localhost:3001',
+                    description: 'backend server for PixelPotion'
+                }
+            ], */
+            apis: ["./routes/*.ts", "./models/*.ts", "./controller/*.ts"],
+        };
+
+        const swaggerSpec = swaggerJsDoc(options);
+        this.app.use("/docs", serve, setup(swaggerSpec));
     }
 
     private initializeCache() {
